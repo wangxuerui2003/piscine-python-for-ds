6d695a612341044701c03f7000e42f34070a003f
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 06:10:33 +0000

    use a better comparison method

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -2,11 +2,11 @@ def all_thing_is_obj(object: any) -> int:
 	obj_type = type(object)
 	obj_type_name: str = obj_type.__name__
 	
-	if obj_type_name == 'str':
+	if obj_type == str:
 		print(f'{object} is in the kitchen : {obj_type}')
 		return 42
 	
-	if obj_type_name in ['list', 'dict', 'set', 'tuple']:
+	if obj_type in [list, dict, set, tuple]:
 		print(f'{obj_type_name.title()} : {obj_type}')
 		return 42
 
