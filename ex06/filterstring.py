24972ae25d9d7c0d27de517f4a51e4a243ba523e
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 07:58:24 +0000

    Norm ex06

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -1,14 +1,25 @@
 from ft_filter import ft_filter
 import sys
 
-try:
-    assert len(sys.argv) == 3, "the arguments are bad"
+
+def main():
+    """
+        Main function
+    """
 
     try:
-        length = int(sys.argv[2])
-        print(ft_filter(lambda word: len(word) > length, sys.argv[1].split()))
-    except ValueError:
-        raise AssertionError("the arguments are bad")
+        assert len(sys.argv) == 3, "the arguments are bad"
 
-except AssertionError as e:
-    print("AssertionError:", e)
+        try:
+            length = int(sys.argv[2])
+            words_list = sys.argv[1].split()
+            print(ft_filter(lambda word: len(word) > length, words_list))
+        except ValueError:
+            raise AssertionError("the arguments are bad")
+
+    except AssertionError as e:
+        print("AssertionError:", e)
+
+
+if __name__ == '__main__':
+    main()
--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -2,4 +2,8 @@ from typing import Iterable, Callable
 
 
 def ft_filter(func: Callable, words: Iterable) -> list[str]:
+    """
+        Returns a list of words which satisfies the func
+    """
+
     return [word for word in words if func(word)]
