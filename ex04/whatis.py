608deb92b0d6e8bbdb24c672e38f26b80d5cfb19
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 16:35:09 +0800

    Fix flake8 errors

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,20 +1,19 @@
 import sys
 
 try:
-	assert len(sys.argv) <= 2, "more than one argument is provided"
+    assert len(sys.argv) <= 2, "more than one argument is provided"
 
-	if len(sys.argv) < 2:
-		sys.exit()
+    if len(sys.argv) < 2:
+        sys.exit()
 
-	is_num = True
-	try:
-		num = int(sys.argv[1])
-		print("I'm Even" if num % 2 == 0 else "I'm Odd")
-	except:
-		is_num = False
+    is_num = True
+    try:
+        num = int(sys.argv[1])
+        print("I'm Even" if num % 2 == 0 else "I'm Odd")
+    except ValueError:
+        is_num = False
 
-	assert is_num, "argument is not an integer"
-	
+    assert is_num, "argument is not an integer"
+
 except AssertionError as e:
-	print("AssertionError:", e)
-
+    print("AssertionError:", e)
