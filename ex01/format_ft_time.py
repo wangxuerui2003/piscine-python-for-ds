6d707902855a34e8411e1821c1c8071c30b00c2f
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 05:43:55 +0000

    ex01 done

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,6 @@
+import time
+from datetime import datetime
+
+seconds_since_epoch = datetime.now().timestamp()
+print(f'Seconds since January 1, 1970: {seconds_since_epoch:,.4f} or {seconds_since_epoch:.2E} in scientific notation')
+print(datetime.now().strftime('%b %d %Y'))
