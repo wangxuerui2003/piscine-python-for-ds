15d560e4fbe3d00629416de290331e5a31863d1d
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 06:49:51 +0000

    AssertionError output made better

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,6 +1,10 @@
 import sys
 
-assert len(sys.argv) <= 2, "more than one argument is provided"
+try:
+	assert len(sys.argv) <= 2, "more than one argument is provided"
+except AssertionError as e:
+	print("AssertionError:", e)
+	sys.exit()
 
 if len(sys.argv) < 2:
 	sys.exit()
@@ -9,5 +13,5 @@ try:
 	num = int(sys.argv[1])
 	print("I'm Even" if num % 2 == 0 else "I'm Odd")
 except:
-	assert False, "argument is not an integer"
+	print("AssertionError:", "argument is not an integer")
 
