83a9bc0fc0930ccae2d40ea1945a52cbeeffd028
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 18:16:10 +0800

    Add special character checking

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -12,7 +12,10 @@ def main():
 
         try:
             length = int(sys.argv[2])
+
             words_list = sys.argv[1].split()
+            for word in words_list:
+                assert word.isalnum(), "the arguments are bad"
             print(ft_filter(lambda word: len(word) > length, words_list))
         except ValueError:
             raise AssertionError("the arguments are bad")
