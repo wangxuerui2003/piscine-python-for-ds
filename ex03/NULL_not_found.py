0b828af544daa7e3f1ed45130a7e3a9a69479c57
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 06:21:38 +0000

    ex03 done

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,23 @@
+from math import isnan
+
+def NULL_not_found(object: any) -> int:
+	obj_type = type(object)
+
+	if object is None:
+		print(f"Nothing: {object} {obj_type}")
+		return 0
+	elif obj_type == float and isnan(object):
+		print(f"Cheese: {object} {obj_type}")
+		return 0
+	elif obj_type == int and object == 0:
+		print(f"Zero: {object} {obj_type}")
+		return 0
+	elif obj_type == str and object == '':
+		print(f"Empty: {object} {obj_type}")
+		return 0
+	elif obj_type == bool and not object:
+		print(f"Fake: {object} {obj_type}")
+		return 0
+	
+	print("Type not found")
+	return 1
