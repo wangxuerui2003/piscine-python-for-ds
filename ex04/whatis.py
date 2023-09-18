9142045c32ddd1c33a1fb8c26b7309f907fc395b
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 07:03:24 +0000

    refine ex04

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -2,16 +2,19 @@ import sys
 
 try:
 	assert len(sys.argv) <= 2, "more than one argument is provided"
+
+	if len(sys.argv) < 2:
+		sys.exit()
+
+	is_num = True
+	try:
+		num = int(sys.argv[1])
+		print("I'm Even" if num % 2 == 0 else "I'm Odd")
+	except:
+		is_num = False
+
+	assert is_num, "argument is not an integer"
+	
 except AssertionError as e:
 	print("AssertionError:", e)
-	sys.exit()
-
-if len(sys.argv) < 2:
-	sys.exit()
-
-try:
-	num = int(sys.argv[1])
-	print("I'm Even" if num % 2 == 0 else "I'm Odd")
-except:
-	print("AssertionError:", "argument is not an integer")
 
