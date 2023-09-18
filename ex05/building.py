27c5c190281118308eedaff911fe4b4ea1f2e822
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-18 07:26:23 +0000

    Fix python norm

--- ex05/building.py
+++ ex05/building.py
@@ -1,67 +1,72 @@
 import sys
 
+
 def count_upper(string: str) -> int:
-	"""
-		return the number of upper letters in a string
-	"""
-	
-	return sum(1 for c in string if c.isupper())
+    """
+        return the number of upper letters in a string
+    """
+
+    return sum(1 for c in string if c.isupper())
+
 
 def count_lower(string: str) -> int:
-	"""
-		return the number of lower letters in a string
-	"""
-	
-	return sum(1 for c in string if c.islower())
+    """ return the number of lower letters in a string """
+
+    return sum(1 for c in string if c.islower())
+
 
 def count_spaces(string: str) -> int:
-	"""
-		return the number of spaces in a string
-	"""
-	
-	return sum(1 for c in string if c.isspace())
+    """
+        return the number of spaces in a string
+    """
+
+    return sum(1 for c in string if c.isspace())
+
 
 def count_digits(string: str) -> int:
-	"""
-		return the number of digits in a string
-	"""
-	
-	return sum(1 for c in string if c.isdigit())
+    """
+        return the number of digits in a string
+    """
+
+    return sum(1 for c in string if c.isdigit())
+
 
 def count_punctuations(string: str) -> int:
-	"""
-		return the number of punctuations in a string
-	"""
+    """
+        return the number of punctuations in a string
+    """
 
-	punctuation_chars = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-	return sum(1 for c in string if c in punctuation_chars)
+    punctuation_chars = '''!()-[]{};:'"\\,<>./?@#$%^&*_~'''
+    return sum(1 for c in string if c in punctuation_chars)
+
 
 def main():
-	"""
-		Main function
-	"""
+    """
+        Main function
+    """
 
-	try:
-		assert len(sys.argv) <= 2, "more than one argument is provided"
-	except AssertionError as e:
-		print("AssertionError:", e)
-		sys.exit()
+    try:
+        assert len(sys.argv) <= 2, "more than one argument is provided"
+    except AssertionError as e:
+        print("AssertionError:", e)
+        sys.exit()
 
-	string = ''
-	if len(sys.argv) < 2:
-		try:
-			string = input("What is the text to count?\n")
-		except EOFError:
-			pass
-	else:
-		string = sys.argv[1]
+    string = ''
+    if len(sys.argv) < 2:
+        try:
+            string = input("What is the text to count?\n")
+        except EOFError:
+            pass
+    else:
+        string = sys.argv[1]
 
-	print(f"The text contains {len(string)} characters!")
-	print(count_upper(string), "upper letters")
-	print(count_lower(string), "lower letters")
-	print(count_punctuations(string), "punctuation marks")
-	print(count_spaces(string), "spaces")
-	print(count_digits(string), "digits")
+    print(f"The text contains {len(string)} characters!")
+    print(count_upper(string), "upper letters")
+    print(count_lower(string), "lower letters")
+    print(count_punctuations(string), "punctuation marks")
+    print(count_spaces(string), "spaces")
+    print(count_digits(string), "digits")
+
 
 if __name__ == '__main__':
-	main()
+    main()
