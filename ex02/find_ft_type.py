aad3492267bf09307ea1ab1a88f111d8ddf68fed
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-19 07:29:15 +0000

    Use isinstance instead of type == type

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -2,7 +2,7 @@ def all_thing_is_obj(object: any) -> int:
 	obj_type = type(object)
 	obj_type_name: str = obj_type.__name__
 	
-	if obj_type == str:
+	if isinstance(object, str):
 		print(f'{object} is in the kitchen : {obj_type}')
 	elif isinstance(object, (list, dict, tuple, set)):
 		print(f'{obj_type_name.title()} : {obj_type}')
