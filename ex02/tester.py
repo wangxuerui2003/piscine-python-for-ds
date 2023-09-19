ee5e1b564fe16184a2d82820b894c63572370b2c
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-19 10:51:45 +0000

    add more test cases

--- ex02/tester.py
+++ ex02/tester.py
@@ -4,10 +4,10 @@ print(ft_load("../landscape.jpg"))
 print(ft_load("../animal.jpeg"))
 print(ft_load("../mario.png"))
 # print(ft_load("../doge.jpg"))
-# print(ft_load("../haha.jpg"))
 # print(ft_load("../cat-meme.jpg"))
 
 """ Invalid path"""
 print(ft_load("../notexist.png"))
 print(ft_load("tester.py"))
+print(ft_load("tester.py.png"))
 print(ft_load(123))
