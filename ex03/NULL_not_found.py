211b0bf8d5e1b4279e4f9fa8331db5355222e551
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-19 13:22:46 +0800

    Use isinstance instead of type == type

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -3,19 +3,15 @@ def NULL_not_found(object: any) -> int:
 
 	if object is None:
 		print(f"Nothing: {object} {obj_type}")
-		return 0
-	elif obj_type == float and str(object) == 'nan':
+	elif isinstance(object, float) and str(object) == 'nan':
 		print(f"Cheese: {object} {obj_type}")
-		return 0
-	elif obj_type == int and object == 0:
+	elif isinstance(object, int) and object == 0:
 		print(f"Zero: {object} {obj_type}")
-		return 0
-	elif obj_type == str and object == '':
+	elif isinstance(object, str) and object == '':
 		print(f"Empty: {object} {obj_type}")
-		return 0
-	elif obj_type == bool and not object:
+	elif isinstance(object, bool) and not object:
 		print(f"Fake: {object} {obj_type}")
-		return 0
-	
-	print("Type not found")
-	return 1
+	else:
+		print("Type not found")
+		return 1
+	return 0
