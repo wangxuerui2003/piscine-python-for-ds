ce7244701866a1d79118ff2af8404cec8596e555
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-09-19 07:28:17 +0000

    Change time format

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -2,5 +2,5 @@ import time
 from datetime import datetime
 
 seconds_since_epoch = datetime.now().timestamp()
-print(f'Seconds since January 1, 1970: {seconds_since_epoch:,.4f} or {seconds_since_epoch:.2E} in scientific notation')
+print(f'Seconds since January 1, 1970: {seconds_since_epoch:,.4f} or {seconds_since_epoch:.2e} in scientific notation')
 print(datetime.now().strftime('%b %d %Y'))
