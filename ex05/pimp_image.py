081e156eb898ed5b16afb586f05979e7705c89b2
wangxuerui2003 <69043064+wangxuerui2003@users.noreply.github.com>
2023-09-20 06:49:34 +0000

    Change the functions to what subject.pdf required

--- ex05/pimp_image.py
+++ ex05/pimp_image.py
@@ -1,55 +1,79 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def show_img(img_arr: np.ndarray, filename: str, extension='jpg'):
+    """
+        Helper function.
+        Use matplotlib to plot the image and save to filename.extension
+    """
+
+    img = Image.fromarray(img_arr)
+    plt.imshow(img)
+    plt.savefig(f"{filename}.{extension}")
 
 
 def ft_invert(array: np.ndarray) -> np.ndarray:
-    """
-        Inverts the color of the image received.
-    """
+    """Inverts the color of the image received."""
 
+    print("The shape of image is:", array.shape)
     copy = array.copy()
     copy[:, :, :] = 255 - copy[:, :, :]
+    print(copy)
+    print("The shape of image after filter is:", copy.shape)
     return copy
 
 
 def ft_red(array: np.ndarray) -> np.ndarray:
-    """
-        Retain only the red color channel
-    """
+    """Retain only the red color channel"""
 
+    print("The shape of image is:", array.shape)
     copy = array.copy()
     copy[:, :, 1] = 0  # ignore green channel
     copy[:, :, 2] = 0  # ignore blue channel
+    print(copy)
+    print("The shape of image after filter is:", copy.shape)
     return copy
 
 
 def ft_green(array: np.ndarray) -> np.ndarray:
-    """
-        Retain only the green color channel
-    """
+    """Retain only the green color channel"""
 
+    print("The shape of image is:", array.shape)
     copy = array.copy()
     copy[:, :, 0] = 0  # ignore red channel
     copy[:, :, 2] = 0  # ignore blue channel
+    print(copy)
+    print("The shape of image after filter is:", copy.shape)
     return copy
 
 
 def ft_blue(array: np.ndarray) -> np.ndarray:
-    """
-        Retain only the blue color channel
-    """
+    """Retain only the blue color channel"""
 
+    print("The shape of image is:", array.shape)
     copy = array.copy()
     copy[:, :, 0] = 0  # ignore red channel
     copy[:, :, 1] = 0  # ignore green channel
+    print(copy)
+    print("The shape of image after filter is:", copy.shape)
     return copy
 
 
 def ft_grey(array: np.ndarray) -> np.ndarray:
-    """
-        Turn an image into greyscale
-    """
+    """Turn an image into greyscale"""
 
+    print("The shape of image is:", array.shape)
+
+    # for each pixel, take the mean value of its rgb value
+    # and put all into a 2D np array
     average: np.ndarray = np.mean(array, axis=2).astype(int)
     copy = array.copy()
+
+    # add an empty 3rd axis to perform the assignment
     copy[:, :, :3] = average[:, :, np.newaxis]
+
+    print(copy)
+    print("The shape of image after filter is:", copy.shape)
     return copy
--- ex05/tester.py
+++ ex05/tester.py
@@ -1,52 +1,59 @@
+import sys
 from pimp_image import ft_blue
 from pimp_image import ft_green
 from pimp_image import ft_red
 from pimp_image import ft_invert
 from pimp_image import ft_grey
+from pimp_image import show_img
 from load_image import ft_load
 from PIL import Image
-import numpy as np
 import matplotlib.pyplot as plt
-
-
-def show_img(img_arr: np.ndarray, filename: str, extension='jpg'):
-    """
-        Helper function.
-        Use matplotlib to plot the image and save to filename.extension
-    """
-
-    img = Image.fromarray(img_arr)
-    plt.imshow(img)
-    plt.savefig(f"{filename}.{extension}")
 
 
 IMAGE_PATH = '../landscape.jpg'
 EXTENSION = 'jpg'
 
-""" Original image"""
-img = Image.open(IMAGE_PATH)
-plt.imshow(img)
-plt.savefig(f'original.{EXTENSION}')
-img.close()
+try:
+    # save the original image
+    img = Image.open(IMAGE_PATH)
+    plt.imshow(img)
+    plt.savefig(f'original.{EXTENSION}')
+    img.close()
+except FileNotFoundError:
+    print("Image not found!")
+    sys.exit()
 
 img_arr = ft_load(IMAGE_PATH)
 
-""" Red filter """
+print()
+print('""" Red filter """')
 red_arr = ft_red(img_arr)
 show_img(red_arr, 'red', EXTENSION)
 
-""" Green filter """
+print()
+print('""" Green filter """')
 green_arr = ft_green(img_arr)
 show_img(green_arr, 'green', EXTENSION)
 
-""" Blue filter """
+print()
+print('""" Blue filter """')
 blue_arr = ft_blue(img_arr)
 show_img(blue_arr, 'blue', EXTENSION)
 
-""" Invert filter """
+print()
+print('""" Invert filter """')
 invert_arr = ft_invert(img_arr)
 show_img(invert_arr, 'invert', EXTENSION)
 
-""" Grey filter """
+print()
+print('""" Grey filter """')
 grey_arr = ft_grey(img_arr)
 show_img(grey_arr, 'grey', EXTENSION)
+
+print()
+print('""" Docstrings """')
+print("ft_blue:", ft_blue.__doc__)
+print("ft_green:", ft_green.__doc__)
+print("ft_red:", ft_red.__doc__)
+print("ft_grey:", ft_grey.__doc__)
+print("ft_invert:", ft_invert.__doc__)
