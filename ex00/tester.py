e50680e6ec8a2256324bdb85cc63e71e0e1a5b26
wangxuerui2003 <69043064+wangxuerui2003@users.noreply.github.com>
2023-09-20 05:52:58 +0000

    Refine ex00 tester

--- ex00/tester.py
+++ ex00/tester.py
@@ -1,7 +1,7 @@
 from give_bmi import give_bmi, apply_limit
 
 
-""" Basic test """
+print('""" Basic test """')
 height = [2.71, 1.15]
 weight = [165.3, 38.4]
 
@@ -9,35 +9,35 @@ bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
 print(apply_limit(bmi, 26))
 
-""" give_bmi different size test """
 print()
+print('""" give_bmi different size test """')
 height = [1.75, 1.80, 1.85]
 weight = [123, 123]
 give_bmi(height, weight)
 
-""" Invalid data type test """
 print()
+print('""" Invalid data type test """')
 height = [1.75, 1.80, 'abc']
 weight = [123, 123, 123]
 give_bmi(height, weight)
 apply_limit(['a', 'b'], 26)
 
-""" Dimension too high test """
 print()
+print('""" Dimension too high test """')
 height = [[1.75, 1.80], [1.70, 1.85]]
 weight = [123, 123]
 give_bmi(height, weight)
 apply_limit([[1, 2], [3, 4]], 26)
 
-""" Not same size lists test """
 print()
+print('""" Not same size lists test """')
 height = [[1.75, 1.80, 1], [1.70, 1.85]]
 weight = [123, 123]
 give_bmi(height, weight)
 apply_limit([[1, 2], [3, 4]], 26)
 
-""" Not a list test """
 print()
+print('""" Not a list test """')
 height = 'a'
 weight = [123, 123]
 give_bmi(height, weight)
